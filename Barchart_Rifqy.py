54d7b458b860a03e5cb336659aff00e2f62a8e6c
Rifqy Rivaldi <127075043+RifqyRivaldi@users.noreply.github.com>
2023-06-21 21:31:31 +0700

    Add files via upload

---
+++ Barchart_Rifqy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+#!/usr/bin/env python
+# coding: utf-8
+# Import library yang diperlukan
+import pandas as pd
+import matplotlib.pyplot as plt
+# Membaca database
+data = pd.read_csv("D:/UAS/605603222_bukalapak.csv")
+# Membuat bar chart dengan jumlah_sold sebagai sumbu X dan jumlah_rating sebagai sumbu Y
+plt.bar(data['jumlah_sold'], data['jumlah_rating'])
+# Menambahkan judul ke plot
+plt.title("Jumlah Rating dan Jumlah Sold")
+# Menetapkan label sumbu X dan Y
+plt.xlabel('Jumlah Sold')
+plt.ylabel('Jumlah Rating')
+# Menampilkan plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
